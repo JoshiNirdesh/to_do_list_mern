c3b786c3d02bc2bdcc51be2cb3f8abe5db07b175
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-29 17:38:24 +0545

    login frontend

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import Hero from "../assets/hero.jpg"
+import { Link } from 'react-router-dom'
+import "../components/landing.css"
 const Landing = () => {
   return (
-    <div>
-      This is the Landing Page
+    <div className='hero'>
+      <div className="intro-text">
+        <h1>
+          <span className='tagline1'>Organize work and life</span>
+          <span className="tagline2">finally.</span>
+        </h1>
+        <p>
+          Type just anything into the taks field and TodoList<br/>
+          on-of-its-kind natural language recognization will instantly fill your 
+          to-do-list
+        </p>
+        <Link className="btn red" to='/register'>Register Now !</Link>
+        <Link className="btn blue" to="/login">Login</Link>
+      </div>
+      <div>
+        <img src={Hero} alt="" width={'100%'} height={515} />
+      </div>
     </div>
+    
   )
 }
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import "./login.css"
 
 const Login = () => {
+
+    const [email,setEmail]=useState("");
+
+    const[password,setPassword]=useState("");
+
+    const loginHandler = (e)=>{
+        try {
+            e.preventDefault()
+            alert(`Login Data ${email} ${password}`);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
   return (
-    <div>
-      This is login page
+   <div className="form-container">
+    <div className="form">
+        <div className="mb-3">
+            <i className='fa-solid fa-circle-user'></i>
+        </div>
+        <div className="mb-3">
+            <input type="email" className='form-control'
+            placeholder='enter email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+        </div>
+          <div className="mb-3">
+            <input type="password" className='form-control'
+            placeholder='enter password' calue={password} onChange={(e)=>setPassword(e.target.value)} />
+        </div>
+        <div className="form-bottom">
+            <p className='text-center'>
+                Not a user? please 
+                <Link to="/register">Register</Link>
+            </p>
+            <button type='submit' className='login-btn' onClick={loginHandler}>Login</button>
+        </div>
     </div>
+   </div>
   )
 }
 
