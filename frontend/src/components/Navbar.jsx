b8c801cf7ce663eb7fea1641073bb48142c3e942
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-31 17:07:28 +0545

    navbar dynamic

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const [username, setUsername] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem("to-do"));
+        console.log("username" + userData && userData.user.username);
+        setUsername(userData && userData.user.username)
+    }, [])
+    const logoutHandler = ()=>{
+        localStorage.removeItem('to-do')
+        navigate('/login')
+    }
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
+            <div>
+                <div>
+                    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                        <div className="container-fluid">
+                            <button
+                                className="navbar-toggler"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarTogglerDemo01"
+                                aria-controls="navbarTogglerDemo01"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span className="navbar-toggler-icon" />
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                                <h4 className="navbar-brand">
+                                    <i className="fa-solid fa-user-tie" /> &nbsp;
+                                    <i>Welcome</i> {username}!
+                                </h4>
+                                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <Link
+                                            className="nav-link active"
+                                            aria-current="page"
+                                            to="/home"
+                                        >
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/todoList">
+                                            My todo List
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <button
+                                            className="nav-link "
+                                            title="logout"
+                                            onClick={logoutHandler}
+                                        >
+                                            <i className="fa-solid fa-power-off text-danger fa-2x" />
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
-            </nav>
+            </div>
         </div>
     )
 }
--- frontend/src/components/Todolist.jsx
+++ frontend/src/components/Todolist.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Todolist = () => {
   return (
+
     <div>
+      <Navbar />
       This is the todolist page
     </div>
   )
