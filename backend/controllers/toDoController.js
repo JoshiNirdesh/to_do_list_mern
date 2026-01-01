411646042cd987197d00c79aad800017ef4cb540
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-01-01 11:49:50 +0545

    create todo Controller

--- backend/controllers/toDoController.js
+++ backend/controllers/toDoController.js
@@ -1,9 +1,28 @@
-const createToDoController = (req, res) => {
+const toDoModel = require("../model/toDoModel");
+
+const createToDoController = async (req, res) => {
     try {
-        const{title,description,createdBy}
+        const { title, description, createdBy } = req.body;
+        if (!title || !description) {
+            return res.status(500).send({
+                success: false,
+                message: "Please provide title and description"
+            })
+        }
+        const toDo = new toDoModel({ title, description, createdBy });
+        await toDo.save();
+        res.status(201).send({
+            success: true,
+            message: "Todo created successfully",
+            data: toDo
+        });
     } catch (error) {
-        
+        res.send(500).send({
+            success: false,
+            message: "Create todo api error",
+            error
+        })
     }
 
 }
-module.exports=createToDoController;
+module.exports = createToDoController;
